c7a443574c7dc24400456e445122bbb110cad4a2
JVMergulho <jvitormergulho@gmail.com>
2023-07-20 22:57:14 -0300

    nome do arquivo também é enviado

--- client1.py
+++ client1.py
@@ -1,13 +1,18 @@
 from socket import *
-serverName = "192.168.0.148" # ip do sevidor
-serverPort = 12000 # porta para a qual as mensagens são enviadas no servidor
-buffer_size = 1024 # 1024 bytes de buffer -> tamanho do pacote
-file = open('logo.png', 'rb') 
-clientSocket = socket(AF_INET, SOCK_DGRAM) # cria socket para UDP
+serverIP = input("insert server IP: ")  # ip do sevidor
+serverPort = 12000  # porta para a qual as mensagens são enviadas no servidor
+buffer_size = 1024  # 1024 bytes de buffer -> tamanho do pacote
+
+filePath = input('\nPath of the file you want to send: ')
+file = open(filePath, 'rb')
+clientSocket = socket(AF_INET, SOCK_DGRAM)  # cria socket para UDP
+
+filePath_list = filePath.split('\\')  
+clientSocket.sendto(filePath_list[-1].encode(), (serverIP, serverPort)) # enviando nome do arquivo
 
 data = file.read(buffer_size)
 
 while (data):
-    if(clientSocket.sendto(data,(serverName, serverPort))):
+    if (clientSocket.sendto(data, (serverIP, serverPort))):
         print("sending ...")
         data = file.read(buffer_size)
--- server1.py
+++ server1.py
@@ -1,19 +1,32 @@
 from socket import *
-serverPort = 12000 # porta em que vamos receber as mensagens
-serverSocket = socket(AF_INET,SOCK_DGRAM) # cria socket UDP
-serverSocket.bind(('', serverPort)) 
+
+serverIP = gethostbyname(gethostname())
+
+serverPort = 12000  # porta em que vamos receber as mensagens
+serverSocket = socket(AF_INET, SOCK_DGRAM)  # cria socket UDP
+serverSocket.bind(('', serverPort))
 buffer_size = 1024
-file = open('logo_rcv.png','wb')
+
 print('The server is ready to receive')
+print('The server IP is ', serverIP)
+
+data, clientAddr = serverSocket.recvfrom(buffer_size)
+
+name, type = data.decode().split('.')
+fileName = name + '_there.' + type
+file = open(fileName, 'wb')
+
+print(fileName)
 
 while 1:
-    data,clientAddr = serverSocket.recvfrom(buffer_size)
     try:
-        while(data):
+        data, clientAddr = serverSocket.recvfrom(buffer_size)
+        while (data):
             file.write(data)
             serverSocket.settimeout(2)
-            data,clientAddr = serverSocket.recvfrom(buffer_size)
+            data, clientAddr = serverSocket.recvfrom(buffer_size)
     except timeout:
         file.close()
         serverSocket.close()
-        print ('File Downloaded')
+        print('File Downloaded')
+        break
