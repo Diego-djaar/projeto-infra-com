9673131ce7d8d626fd241136fc4cad7c044fedc5
João Vitor Mergulhão <jvitormergulho@gmail.com>
2023-08-06 18:32:16 -0300

    adicionando comentários em client1.py

--- primeira etapa/client1.py
+++ primeira etapa/client1.py
@@ -6,13 +6,13 @@ import math
 # Definições
 serverIP = input("Insira o IP do servidor: ")
 serverPort = 12000
-serverAddr = (serverIP, serverPort)
-buffer_size = 1024
+serverAddr = (serverIP, serverPort) # define tupla com IP e porta de destino
+buffer_size = 1024 # define tamnaho do buffer
 
 filePath = input('\nNome do arquivo: ')
 clientSocket = socket(AF_INET, SOCK_DGRAM)  # cria socket para UDP
 
 print("Enviando...")
-Sendfile(filePath, clientSocket, serverAddr, buffer_size)
+Sendfile(filePath, clientSocket, serverAddr, buffer_size) # envia pacotes
 print("Recebendo...")
-Receivefile(clientSocket, buffer_size)
+Receivefile(clientSocket, buffer_size) # recebe pacotes
