4732c47029883ea1091bc56dbbf185921ebd52a8
João Vitor Mergulhão <jvitormergulho@gmail.com>
2023-08-06 17:24:11 -0300

    Adicionando comentários em file.py

--- primeira etapa/file.py
+++ primeira etapa/file.py
@@ -19,9 +19,9 @@ def Sendfile(filePath: str, clientSocket: socket, serverAddr: tuple[str, int], b
 
         # Enviando conteúdo do arquivo pacote a pacote (cada um tem buffer_size bytes)
         for i in range(0, n_pacotes):
-            if (clientSocket.sendto(data, serverAddr)):
+            if (clientSocket.sendto(data, serverAddr)): # envia data e checa se foi bem sucedido
                 sleep(0.02) # delay entre envio de pacotes
-                data = file.read(buffer_size)
+                data = file.read(buffer_size) # atualiza variável data
 
 # recebe, salva e renomeia arquivo
 def Receivefile(serverSocket: socket, buffer_size: int) -> tuple[str, any]:
@@ -41,8 +41,8 @@ def Receivefile(serverSocket: socket, buffer_size: int) -> tuple[str, any]:
             for i in range(0, n_pacotes):
                 data, clientAddr = serverSocket.recvfrom(buffer_size) # recebe os pacotes por meio do socket serverSocket
                 file.write(data)
-                serverSocket.settimeout(2)
-        except timeout:
+                serverSocket.settimeout(2) # seta tempo para timout como 2 segundos
+        except timeout: # caso ocorra um timeout o arquivo e o socket são fechados
             file.close()
             serverSocket.close()
             print('Timeout')
