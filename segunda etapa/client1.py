2b86f7382b5bfaa3fde1046bf50dbe8983c8d4a0
João Vítor Régis <jvcr@cin.ufpe.br>
2023-09-19 17:15:10 -0300

    Início da implementação do cliente

--- segunda etapa/client1.py
+++ segunda etapa/client1.py
@@ -1,55 +1,32 @@
 from socket import *
-from file import Sendfile, Receivefile
-import os
-import math
-import keyboard
-import time
+from RDT import *
 import threading
 
-def event(e):
-    if e.event_type == keyboard.KEY_DOWN and e.name == 'esc':
-        global exit
-        print("Conexão encerrada.")
-        exit = True
-    elif e.event_type == keyboard.KEY_DOWN and e.name == 's': # Se o input for send
-        global send
-        send = True
+def sendRDT(objRDT: RDT, serverAddr: tuple[str, int], buffer_size: int):
+    global sequence_number, username
+    print("Digite a sua mensagem:")
+    mensagem = str(input())
+    objRDT.sendmsg(f"{mensagem}", 0, serverAddr, buffer_size)
+    
+
+def connectserver(objRDT: RDT, username: str, serverAddr: tuple[str, int], buffer_size: int):
+    global sequence_number
+    objRDT.sendmsg(f"hi, meu nome eh {username}", 0, serverAddr, buffer_size)
+    sequence_number = "1"
+        
 
 # Definições
-serverIP = input("Insira o IP do servidor: ")
+serverIP = input("Insira o IP do servidor: ") # Já vai ser conhecido então pode ser implementado sem o input
 serverPort = 12000
 serverAddr = (serverIP, serverPort) # define tupla com IP e porta de destino
 buffer_size = 1024 # define tamnaho do buffer
 clientSocket = socket(AF_INET, SOCK_DGRAM)  # cria socket para UDP
 sequence_number = "0"
 
-# Máquina de estados
-state = "call0"
-exit = False
-print("Aperte a tecla s para enviar um arquivo ou esc para sair")
-while (not exit):
-    # Esperando input do teclado para encerrar a conexão
-    keyboard.hook(event)
+username = str(input("Qual o seu nome de usuário?"))
+conexaoRDT = RDT(clientSocket)
+connectserver(conexaoRDT, username, serverAddr, buffer_size)
+print("Você está conectado ao servidor. Caso queira encerrar a conexão digite bye.")
+print("Para ter acesso à lista de usuários digite o comando list.")
 
-    if (state == "call0"):
-        if (send):
-            filePath = input('\nNome do arquivo: ')
-            print("Enviando...")
-            Sendfile(filePath, clientSocket, serverAddr, buffer_size) # envia pacotes
-            state = "waitACK0"
-
-    elif (state == "waitACK0"):
-        print("Recebendo...")
-        Receivefile(clientSocket, buffer_size, sequence_number) # recebe pacotes
-        if sequence_number == "1":
-            print("Reenviando...")
-            Sendfile(filePath, clientSocket, serverAddr, buffer_size) # envia pacotes
-        # Falta lidar com timeout e testar
-    elif (state == "call1"):
-
-    elif (state == "waitACK1"):
-
-
-
-# print("Recebendo...")
-# Receivefile(clientSocket, buffer_size) # recebe pacotes
+# Iniciar as threads de receber e enviar dados
